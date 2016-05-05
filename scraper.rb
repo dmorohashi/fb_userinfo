a86b5b572bad97e702985e40fdd4d8eb733eccb7
dmorohashi <daisuke.morohashi@gmail.com>
2016-05-05 11:07:29 +0900

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,21 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
+
+agent = Mechanize.new
+# Read in a page
+page = agent.get("http://foo.com")
 #
-# agent = Mechanize.new
+# Find somehing on the page using css selectors
+p page.at('div.content')
 #
-# # Read in a page
-# page = agent.get("http://foo.com")
+# Write out to the sqlite database using scraperwiki library
+ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
 #
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# An arbitrary query against the database
+ScraperWiki.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the Mechanize or ScraperWiki libraries.
 # You can use whatever gems you want: https://morph.io/documentation/ruby
